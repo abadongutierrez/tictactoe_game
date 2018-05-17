3cdc09096a5253fbc98def1de08fe9cdfbdc32dc
abadon.gutierrez <abadon.gutierrez@gmail.com>
2018-05-16 21:31:43 -0500

    Interactive Squares

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,14 +26,16 @@ import ReactDOM from "react-dom"
 class Square extends React.Component {
     render() {
         return (
-            <button className = "square">{ /* TODO */ }</button>
+            <button className = "square" onClick={() => alert('clicked on ' + this.props.value)}>
+                {this.props.value}
+            </button>
         );
     }
 }
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i} />;
     }
 
     render() {
